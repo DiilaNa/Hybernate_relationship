252dd033c5fc00111000a7454c314c352b72adb2
DiilaNa <liyanaarachchidilan@gmail.com>
2025-02-20 11:43:19 +0530

    OneToMany Relationship file structure added

--- One-to-Many/src/main/java/Entity/Customer.java
+++ One-to-Many/src/main/java/Entity/Customer.java
@@ -9,19 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Customer_table")
+@Table(name = "Customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //    AUTO - hibernate decided auto
-//    IDENTY - databases genarate auto
-//    For custom value dont juse @GeneratedValue
-//    Auto hari IDENTY hari danawanm eka int ekak wiya yuthuyi
     private int id;
     private String name;
 
-    @OneToOne
-    @JoinColumn(name = "nic_number")
-    private IDCard idCard;
 
 }
